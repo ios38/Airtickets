c74337146fc489077518551d3f22caa43924e57f
Maksim Romanov <ios38ru@gmail.com>
2020-05-17 18:44:37 +0800

    Initial Commit

---
+++ Airtickets/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Airtickets
+//
+//  Created by Maksim Romanov on 17.05.2020.
+//  Copyright © 2020 Maksim Romanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Airtickets/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Airtickets
+//
+//  Created by Maksim Romanov on 17.05.2020.
+//  Copyright © 2020 Maksim Romanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
